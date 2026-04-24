ebf31d46eed8e4a63a0f014347c18be17755b1e1
sava-kuli4 <savakuleshov9@gmail.com>
2026-04-24 16:27:48 +0500

    Add files via upload

---
+++ 1myjava.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/sava-kuli4/steamdeck/refs/heads/final/Tyler.png",
+
+  "https://raw.githubusercontent.com/sava-kuli4/steamdeck/refs/heads/final/Drake.png",
+
+  "https://raw.githubusercontent.com/sava-kuli4/steamdeck/refs/heads/final/JPEGMAFIA.png",
+
+  "https://raw.githubusercontent.com/sava-kuli4/steamdeck/refs/heads/final/Kanye.png",
+
+  "https://raw.githubusercontent.com/sava-kuli4/steamdeck/refs/heads/final/Wu-Tang.png",
+
+  "https://raw.githubusercontent.com/sava-kuli4/steamdeck/refs/heads/final/DOOM.png"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "Chromakopia",
+
+  "Dark Lane Demo Tapes",
+
+  "I Lay Down My Life for You",
+
+  "The College Dropout",
+
+  "Enter The 36 Chambers",
+
+  "Madvillainy"
+
+];
